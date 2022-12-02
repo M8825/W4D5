c38b2557e66b0097c1aee19a7bf4e7d77a843583
Mlkz <maho.mamulashvili@gmail.com>
2022-12-02 14:59:53 -0500

    subsums

--- execution_time_diff/index.rb
+++ execution_time_diff/index.rb
@@ -18,8 +18,8 @@ def my_min_two(arr)
    smallest
 end
 
- list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min_two(list)
+#  list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_min_two(list)
 
 
 def largest_contiguous_subsum(arr)
@@ -39,7 +39,26 @@ def largest_contiguous_subsum(arr)
    max_subArray
 end
 
-   list = [5, 3, -7]
-    p largest_contiguous_subsum(list) # => 8
+# list = [5, 3, -7]
+# p largest_contiguous_subsum(list) # => 8
 
- 
+
+def largest_contiguous_subsum(arr)
+   largest = arr.first
+   curr = 0
+
+   arr.each do |ele|
+      curr += ele
+      largest = curr if curr > largest 
+      curr = 0 if curr < 0 
+   end
+
+   largest
+end
+
+
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list)
+
+list = [-5, -1, -3]
+p largest_contiguous_subsum(list) # => -1 (from [-1])
